148b94d48ee76cb7cb3167ccb6dd8d6010e67e9b
Vencejo Software <vencejosoftware@gmail.com>
2017-12-06 14:00:34 -0300

    Added dependency resolver

---
+++ code/get_dependencies.bat
@@ -0,0 +1,51 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+
+set path_3rd_party=..\..\3rd_party
+
+if exist "C:\Program Files (x86)\Embarcadero\Studio\19.0\bin\rsvars.bat" (
+  set ide_path="C:\Program Files (x86)\Embarcadero\Studio\19.0\bin"
+) else if exist "C:\Program Files (x86)\Embarcadero\RAD Studio\7.0\bin\" (
+  set ide_path="C:\Program Files (x86)\Embarcadero\RAD Studio\7.0\bin"
+)
+
+@echo "Running ambient vars..."
+call !ide_path!\rsvars.bat
+
+if not exist %path_3rd_party% (
+  mkdir %path_3rd_party%
+)
+
+if not exist %path_3rd_party%\dxDUnit\ (
+  @echo "Clonning dxDUnit..."
+  git clone https://github.com/VencejoSoftware/dxDUnit.git %path_3rd_party%\dxDUnit\
+)
+if not exist %path_3rd_party%\dunit-agiledoxtestrunner\ (
+  @echo "Clonning dunit-agiledoxtestrunner..."
+  git clone https://github.com/VencejoSoftware/delphi-dunit-agiledoxtestrunner.git %path_3rd_party%\dunit-agiledoxtestrunner\
+)
+if not exist %path_3rd_party%\CodeCoverage\ (
+  @echo "Clonning CodeCoverage..."
+  git clone https://github.com/VencejoSoftware/codecoverage.git %path_3rd_party%\CodeCoverage\
+)
+if not exist %path_3rd_party%\CodeCoverage\CodeCoverage.exe (
+  @echo "Building CodeCoverage..."
+  set current="%cd%"
+  cd %path_3rd_party%\CodeCoverage\
+  call build.bat
+  move build\Win32\CodeCoverage.exe .\
+  cd "%current%"
+)
+if not exist %path_3rd_party%\graphviz\ (
+  @echo "graphviz..."
+  git clone https://github.com/VencejoSoftware/graphviz.git %path_3rd_party%\graphviz\
+)
+if not exist %path_3rd_party%\pasdoc\ (
+  @echo "pasdoc..."
+  git clone https://github.com/VencejoSoftware/pasdoc.git %path_3rd_party%\pasdoc\
+)
+if not exist %path_3rd_party%\multimarkdown\ (
+  @echo "multimarkdown..."
+  git clone https://github.com/VencejoSoftware/multimarkdown.git %path_3rd_party%\multimarkdown\
+)
