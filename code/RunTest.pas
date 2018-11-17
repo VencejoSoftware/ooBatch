39dd985e7216288ac4ddae3251d6f4ead96b9a14
Vencejo Software <vencejosoftware@gmail.com>
2018-11-17 08:06:33 -0300

    + Fix units names

--- code/RunTest.pas
+++ code/RunTest.pas
@@ -3,7 +3,7 @@
   Distributed under the terms of the Modified BSD License
   The full license is distributed with this software
 }
-unit ooRunTest;
+unit RunTest;
 //{$IFNDEF FPC}
 {$APPTYPE CONSOLE}
 //{$ENDIF}
