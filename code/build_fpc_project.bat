0455206a2227a0b1e6b724c26bda579ef84e5896
Vencejo Software <vencejosoftware@gmail.com>
2020-02-01 11:07:04 -0300

    Fixed warning message when compile fpc project

--- code/build_fpc_project.bat
+++ code/build_fpc_project.bat
@@ -7,7 +7,4 @@ set config_mode=%3%
 set project_path=Lazarus
 set fpc_path=C:\lazarus\
 
-%fpc_path%\lazbuild --build-mode=%config_mode% "%path_project%\!project_path!\%project%"
-  --build-all
-  --os=win32
-  --cpu=i386
+%fpc_path%\lazbuild --build-mode=%config_mode% "%path_project%\!project_path!\%project%" --build-all
